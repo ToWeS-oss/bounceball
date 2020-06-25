db61690fe89d331e342dbf84df4c372cf2378561
Mykol Harper <mykol.harper@outlook.com>
2020-06-25 17:54:00 +0100

    walls and pads

---
+++ bounceball/Assets/PadCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadCode : MonoBehaviour
+{
+    private Rigidbody2D _pads;
+    float width = Screen.width;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
